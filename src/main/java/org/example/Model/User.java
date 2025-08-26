91134eb754939f00ecb884876b11a47f8028986e
Lahcen <161513351+Ahtat204@users.noreply.github.com>
2025-08-26 18:12:00 +0200

    Update User.java

--- src/main/java/org/example/Model/User.java
+++ src/main/java/org/example/Model/User.java
@@ -3,7 +3,7 @@ package org.example.Model;
 /**
 instead of creating a class and overriding equals , hashcode and toString ,
  Java offers the record type ,
- which is like a class except that it is a value type not a reference type ,
+ which is like a class except that it comes with Overriden Equals(),hashcode() and ToString()  ,
  almost like Kotlin's data class and C# record
  * @param Email the user's email address
  * @param Password the user's password
